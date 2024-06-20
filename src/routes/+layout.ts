460fee95e83c73ed875bd131e79f63749e6cdade
Anthony Fok <anthony.fok@nrcan-rncan.gc.ca>
2024-06-20 07:56:31 -0600

    Set up PostHog survey

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,2 +1,16 @@
 export const prerender = true;
 export const trailingSlash = 'always';
+
+import posthog from 'posthog-js'
+import { browser } from '$app/environment';
+
+export const load = async () => {
+
+  if (browser) {
+    posthog.init(
+      'phc_gXl8K7Gu7YCBU5KH6sO8pFy0InNYRWngGdZ4o23KM07',
+      { api_host: 'https://us.i.posthog.com' }
+    )
+  }
+  return
+};
