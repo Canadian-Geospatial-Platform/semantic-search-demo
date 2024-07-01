6a5158d5a80cf76114cde8f22c0049884e98b73f
Anthony Fok <anthony.fok@nrcan-rncan.gc.ca>
2024-07-01 06:56:48 -0600

    Modify tailwind.config.ts for Skeleton
    https://www.skeleton.dev/docs/get-started#tailwind-configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,28 @@
+import { join } from 'path';
 import type { Config } from 'tailwindcss';
 
+// 1. Import the Skeleton plugin
+import { skeleton } from '@skeletonlabs/tw-plugin';
+
 export default {
-	content: ['./src/**/*.{html,js,svelte,ts}'],
+	// 2. Opt for dark mode to be handled via the class method
+	darkMode: 'class',
+
+	content: [
+		'./src/**/*.{html,js,svelte,ts}',
+		// 3. Append the path to the Skeleton package
+		join(require.resolve(
+			'@skeletonlabs/skeleton'),
+			'../**/*.{html,js,svelte,ts}'
+		)
+	],
 
 	theme: {
 		extend: {}
 	},
 
-	plugins: []
+	plugins: [
+		// 4. Append the Skeleton plugin (after other plugins)
+		skeleton
+	]
 } as Config;
