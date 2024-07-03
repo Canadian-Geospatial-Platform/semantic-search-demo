d00c77b64d873656b18b6f684e0910789d6770c2
Anthony Fok <anthony.fok@nrcan-rncan.gc.ca>
2024-07-03 16:32:56 -0600

    Uninstall vite-plugin-tailwind-purgecss

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { purgeCss } from 'vite-plugin-tailwind-purgecss';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-	plugins: [sveltekit(), purgeCss()],
+	plugins: [sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
