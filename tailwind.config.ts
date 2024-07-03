720f57fdf32ec2e5bc4d56287f01b2745229cb5d
Anthony Fok <anthony.fok@nrcan-rncan.gc.ca>
2024-07-03 16:23:24 -0600

    Revert "Modify tailwind.config.ts for Skeleton"
    This reverts commit 6a5158d5a80cf76114cde8f22c0049884e98b73f.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,29 +1,11 @@
-import { join } from 'path';
 import type { Config } from 'tailwindcss';
 
-// 1. Import the Skeleton plugin
-import { skeleton } from '@skeletonlabs/tw-plugin';
-
 export default {
-	// 2. Opt for dark mode to be handled via the class method
-	darkMode: 'class',
-
-	content: [
-		'./src/**/*.{html,js,svelte,ts}',
-		// 3. Append the path to the Skeleton package
-		join(require.resolve(
-			'@skeletonlabs/skeleton'),
-			'../**/*.{html,js,svelte,ts}'
-		)
-	],
+	content: ['./src/**/*.{html,js,svelte,ts}'],
 
 	theme: {
 		extend: {}
 	},
 
-	plugins: [
-		require("@tailwindcss/typography"),
-		// 4. Append the Skeleton plugin (after other plugins)
-		skeleton
-	]
+	plugins: []
 } as Config;
