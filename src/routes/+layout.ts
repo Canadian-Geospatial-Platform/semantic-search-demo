c720daa81a516ee22768525f9e68421d6ea421f4
Anthony Fok <anthony.fok@nrcan-rncan.gc.ca>
2024-07-04 09:32:11 -0600

    Configure PostHog session_recording not to mask inputs
    except password of course.

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -15,7 +15,13 @@ export const load = async () => {
     if (!window.location.host.includes('127.0.0.1') && !window.location.host.includes('localhost')) {
       posthog.init('phc_gXl8K7Gu7YCBU5KH6sO8pFy0InNYRWngGdZ4o23KM07', {
         api_host: 'https://us.i.posthog.com',
-        disable_external_dependency_loading: true // Optional - will ensure we never try to load extensions lazily
+        disable_external_dependency_loading: true, // Optional - will ensure we never try to load extensions lazily
+        session_recording: {
+          maskAllInputs: false,
+          maskInputOptions: {
+            password: true
+          }
+        }
       });
     }
   }
