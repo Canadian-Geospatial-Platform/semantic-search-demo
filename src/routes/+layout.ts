041413f1cfda828858e42de400c16da62d6e535c
Anthony Fok <anthony.fok@nrcan-rncan.gc.ca>
2024-07-04 09:22:39 -0600

    Enable PostHog Session replay
    See https://posthog.com/docs/session-replay/installation

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,16 +1,25 @@
 export const prerender = true;
 export const trailingSlash = 'always';
 
-import posthog from 'posthog-js'
+import "posthog-js/dist/recorder";
+import "posthog-js/dist/surveys";
+// import "posthog-js/dist/exception-autocapture";
+import "posthog-js/dist/tracing-headers";
+import "posthog-js/dist/web-vitals";
+import posthog from 'posthog-js';
+
 import { browser } from '$app/environment';
 
 export const load = async () => {
-
   if (browser) {
-    posthog.init(
-      'phc_gXl8K7Gu7YCBU5KH6sO8pFy0InNYRWngGdZ4o23KM07',
-      { api_host: 'https://us.i.posthog.com' }
-    )
+    if (!window.location.host.includes('127.0.0.1') && !window.location.host.includes('localhost')) {
+      posthog.init('phc_gXl8K7Gu7YCBU5KH6sO8pFy0InNYRWngGdZ4o23KM07', {
+        api_host: 'https://us.i.posthog.com',
+        disable_external_dependency_loading: true // Optional - will ensure we never try to load extensions lazily
+      });
+    }
   }
-  return
+  return;
 };
+
+export const _frontmatter = {}
