6caa2c2eaaf412c4a9c5ecc430105ab96f371e08
Anthony Fok <anthony.fok@nrcan-rncan.gc.ca>
2025-01-24 11:42:20 -0700

    Update semantic search result parsing to match latest API changes
    Previously, the semantic search results were enclosed inside "body",
    but they have been moved up a level for simplicity and consistency
    with keyword search, so the front end parsing needs to be adjusted
    accordingly, namely:
    
        data.body.response → data.response
    
    Thanks to Joost van Ulden, Bo Lu and Xinli Cai for letting me know!
    
    Also, now that both returned_hits and total_hits are available,
    we can show e.g. "1 – 10 of 320 records" now.
    
    Bump version to v0.3.8 for good fortune.
    Happy Lunar New Year, coming soon!
    
    Fixes #6

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,6 +1,6 @@
 <footer class="fluid-container">
 	<hr />
-	<p>GeoDiscovery, Canada Centre for Mapping and Earth Observation, Government of Canada, 2024</p>
+	<p>GeoDiscovery, Canada Centre for Mapping and Earth Observation, Government of Canada, 2024–2025</p>
 </footer>
 
 <style>
