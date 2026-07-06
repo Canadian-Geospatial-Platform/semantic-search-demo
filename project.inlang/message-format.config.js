885a82d9a1be98285306823996bae7f4105c5001
svetlana-esina <svetlana.esina@nrcan-rncan.gc.ca>
2026-07-06 11:01:01 -0400

    Added en-fr support

---
+++ project.inlang/message-format.config.js
@@ -0,0 +1,3 @@
+export default {
+    pathPattern: "./src/i18n/{locale}.json"
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
+import { paraglide } from '@inlang/paraglide-vite';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [sveltekit(), paraglide()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
